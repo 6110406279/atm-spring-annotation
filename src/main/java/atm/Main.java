0632267a0353775d70ae1909318d8ab7194a1f4a
Itsadej Pongsapakpiboon <itsadake.p@ku.th>
2020-09-14 17:37:17 +0700

    first commit

---
+++ src/main/java/atm/Main.java
@@ -0,0 +1,16 @@
+package atm;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
+
+        ATM atm = context.getBean(ATM.class);
+
+        AtmUI atmUI = context.getBean(AtmUI.class);
+        atmUI.run();
+    }
+}
